671387dfa8bd46bdf2e9b7373f4e975e4f1454b8
hitoki <h.nakamura.sys22@morijyobi.ac.jp>
2022-11-10 10:30:47 +0900

    Main.javaの変更

--- Main.java
+++ Main.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        double height = 1.65;
-        double weight = 45.5;
+        for(int i = 0; i < 2; i++){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("身長を入力してください。");
+        double height = sc.nextDouble();
+        System.out.println("体重を入力してください。");
+        double weight = sc.nextDouble();
         double BMI = (weight / height / height);
         double bmi = ((double)Math.round(BMI * 100))/100;
 
-        System.out.println(bmi);    
+        System.out.println(bmi);   } 
 }
 }
+
+
 
